30a920eac6b0e1ea00d2faf6dc236c68211a216f
André Luis <anddreluis98@gmail.com>
2022-02-22 20:31:01 -0300

    mensagem de erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import { useEffect, useState } from "react"
 import styles from './App.scss'
-import {AiFillStar} from 'react-icons/Ai'
-import {BsGithub} from 'react-icons/bs'
+import { AiFillStar } from 'react-icons/Ai'
+import { BsGithub } from 'react-icons/bs'
 function App() {
-  
+
 
   const [repositories, setRepositories] = useState([])
 
   const [user, setUser] = useState('')
 
-  function getRepo(e){
-      e.preventDefault()
-    
-      fetch(`https://api.github.com/users/${user}/repos`)
-        .then(resp => resp.json())
-        .then(data => {setRepositories(data)})
-        .catch((err) => console.log(err)
-       
-        )}
+  function getRepo(e) {
+    e.preventDefault()
+
+    fetch(`https://api.github.com/users/${user}/repos`)
+      .then(resp => resp.json())
+      .then(data => { setRepositories(data) })
+      .catch((err) => console.log(err))
+  }
 
   return (
     <div className="div">
-      <form className="form"> 
-        <input className="input" type="text" onChange = {e => setUser (e.target.value)}  placeholder="Digite o user:"></input>
+      <form className="form">
+        <input className="input" type="text" onChange={e => setUser(e.target.value)} placeholder="Digite o user:"></input>
         <button className="btn" type="Submit" onClick={getRepo}>Enviar</button>
       </form>
-      <div className="reposit">
-      <h1 className="Titulo"><BsGithub/> {repositories[0]?.owner?.login}</h1>
-      <ul className="repos">
-        {repositories.map(repo => {
-          return (
-            <li className="repos" key={repo.full_name}>
-              <p>{repo.name} - {repo.stargazers_count} <AiFillStar/></p>
-            </li>
-          )
-        })}
-      </ul>
-      </div>
-      </div> 
+      {repositories.message === 'Not Found' ? (<h1 className="Erro">O usuário não existe!</h1>) : ((<div className="reposit">
+        <h1 className="Titulo"><BsGithub /> {repositories[0]?.owner?.login}</h1>
+        <ul className="repos">
+          {repositories.map(repo => {
+            return (
+              <li className="repos" key={repo.full_name}>
+                <p>{repo.name} - {repo.stargazers_count} <AiFillStar /></p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>))}
+    </div>
   )
 }
 
